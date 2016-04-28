bb71c516c0ab4d3be10558e9b22d8a35ed58aa84
LimanRPG <liman.vita@gmail.com>
2016-04-29 02:26:25 +0300

    Add test + modif

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,2 +1,5 @@
 class Answer < ActiveRecord::Base
+  belongs_to :question
+
+  validates :body, length: { minimum: 5 }, presence: true
 end
--- spec/models/answer_spec.rb
+++ spec/models/answer_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Answer, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :body }
+  it { should validate_length_of(:body).is_at_least(5) }
+  it { should belong_to(:question) }
 end
