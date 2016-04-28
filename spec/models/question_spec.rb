9227ea2e62c887df831f5f553b4c3582152ea83c
LimanRPG <liman.vita@gmail.com>
2016-04-29 01:02:20 +0300

    Add format doc

--- spec/models/question_spec.rb
+++ spec/models/question_spec.rb
@@ -1,5 +1,10 @@
 require 'rails_helper'
 
 RSpec.describe Question, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should validate_presence_of :title }
+  it { should validate_presence_of :body }
+  it { should have_many(:answers).dependent(:destroy) }
+  it { should validate_length_of(:title).is_at_least(5) }
+  it { should validate_length_of(:body).is_at_least(5) }
+
 end
