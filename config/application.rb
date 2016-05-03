ca6783ec1af68dda6b50f697323b9c2ee0612f0e
LimanRPG <liman.vita@gmail.com>
2016-05-04 00:38:31 +0300

    Ошибка: вместо generations надо generators

--- config/application.rb
+++ config/application.rb
@@ -23,7 +23,7 @@ module Proj
     # Do not swallow errors in after_commit/after_rollback callbacks.
     config.active_record.raise_in_transactional_callbacks = true
 
-    config.generation do |g|
+    config.generators do |g|
       g.test_framework :rspec,
                        fixtures: true,
                        view_spec: false,
