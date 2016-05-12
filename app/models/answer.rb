aff5bfcb9f69ce4de107d3112d380425328f0097
LimanRPG <liman.vita@gmail.com>
2016-05-13 02:08:18 +0300

    Add user_id for questions & answers

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,5 +1,6 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
+  belongs_to :user
 
   validates :body, :question_id , presence: true
   validates :body, length: { minimum: 5 }
---
+++ db/migrate/20160512224807_add_user_id_to_question.rb
@@ -0,0 +1,5 @@
+class AddUserIdToQuestion < ActiveRecord::Migration
+  def change
+    add_belongs_to :questions, :user, index: true
+  end
+end
