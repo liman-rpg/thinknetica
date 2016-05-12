488900e4c67e6bd16e192b89baee0933d28ac197
LimanRPG <liman.vita@gmail.com>
2016-05-13 02:27:16 +0300

    Refactoring

--- app/models/user.rb
+++ app/models/user.rb
@@ -2,6 +2,11 @@ class User < ActiveRecord::Base
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
 
-has_many :questions
-has_many :answers
+  has_many :questions
+  has_many :answers
+
+  def author_of?(object)
+    id==object.user_id
+  end
+
 end
