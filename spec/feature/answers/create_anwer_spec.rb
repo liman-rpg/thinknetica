6ebb3f760d97c890e807cf09afc78eaae26c2111
LimanRPG <liman.vita@gmail.com>
2016-05-29 21:13:10 +0300

    Refactoring

--- spec/feature/answers/create_anwer_spec.rb
+++ spec/feature/answers/create_anwer_spec.rb
@@ -5,19 +5,37 @@ feature 'Create answer', %q{
   Authenticate user
   I want to create answer
 } do
-  given!(:question) { create(:question) }
+  given!(:question) { create(:question, user: user) }
   given (:user) { create(:user) }
 
-  scenario 'Authenticate user try create answer', js: true do
-    sign_in(user)
+  describe 'Authenticate user' do
+    before do
+      sign_in(user)
+      visit question_path(question)
+    end
 
-    visit question_path(question)
-    fill_in 'Ответ', with: 'BodyTestAnswer'
-    click_on 'Reply'
+    scenario 'try create answer', js: true do
+      fill_in 'Ответ', with: 'BodyTestAnswer'
+      click_on 'Reply'
 
-    expect(page).to have_content 'BodyTestAnswer'
-    expect(page).to have_content 'Ответ'
-    expect(current_path).to eq question_path(question)
+      expect(page).to have_content 'BodyTestAnswer'
+      expect(page).to have_content 'Ответ'
+      expect(current_path).to eq question_path(question)
+    end
+
+    scenario 'try create nill answer', js: true do
+      fill_in 'Ответ', with: ''
+      click_on 'Reply'
+
+      expect(page).to have_content "Body can't be blank"
+    end
+
+    scenario 'try create short answer (:body)', js: true do
+      fill_in 'Ответ', with: 'SML'
+      click_on 'Reply'
+
+      expect(page).to have_content "Body is too short (minimum is 5 characters)"
+    end
   end
 
   scenario 'Unauthenticate user try create answer' do
@@ -27,22 +45,5 @@ feature 'Create answer', %q{
 
     expect(page).to have_content 'You need to sign in or sign up before continuing.'
     expect(current_path).to eq new_user_session_path
-  end
-
-  scenario 'Authenticate user try create nill answer', js: true do
-    sign_in(user)
-    visit question_path(question)
-    click_on 'Reply'
-
-    expect(page).to have_content "Body can't be blank"
-  end
-
-   scenario 'Authenticate user try create short answer (:body)', js: true do
-    sign_in(user)
-    visit question_path(question)
-    fill_in 'Ответ', with: 'SML'
-    click_on 'Reply'
-
-    expect(page).to have_content "Body is too short (minimum is 5 characters)"
   end
 end
