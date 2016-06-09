bbd3670435131b8a918726d478246184518f1e8a
LimanRPG <liman.vita@gmail.com>
2016-06-10 00:27:40 +0300

    Polymorphic attachments.Answers & Questions associated with attachment

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,9 +1,12 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
+  has_many :attachments , as: :attachable
 
   validates :body, :question_id, :user_id, presence: true
   validates :body, length: { minimum: 5 }
+
+  accepts_nested_attributes_for :attachments
 
   def set_as_best!
     transaction do
