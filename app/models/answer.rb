cec6bbfe918db77f796c2587adac3e30d94dbad8
LimanRPG <liman.vita@gmail.com>
2016-06-11 22:35:32 +0300

    Area remove file form for answers & questions

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -6,7 +6,7 @@ class Answer < ActiveRecord::Base
   validates :body, :question_id, :user_id, presence: true
   validates :body, length: { minimum: 5 }
 
-  accepts_nested_attributes_for :attachments, reject_if: :all_blank
+  accepts_nested_attributes_for :attachments, reject_if: :all_blank, allow_destroy: true
 
   def set_as_best!
     transaction do
---
+++ spec/feature/questions/remove_file_form_to_question_spec.rb
@@ -0,0 +1,26 @@
+require 'feature_helper'
+
+feature 'Remove files to question', %q{
+  In order to illustrate my question
+  As an question's author
+  I'd like to be remove files
+  } do
+    given(:user) { create(:user) }
+
+    scenario "Authenticate user as author of question try remove question's file form" do
+      sign_in(user)
+      visit new_question_path
+
+      within all('.nested-fields').first do
+        attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+        expect(page).to_not have_content 'spec_helper.rb'
+
+        click_link 'Remove form', "#{Rails.root}/spec/spec_helper.rb"
+      end
+
+      within '.attachments-form' do
+        expect(page).to_not have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      end
+    end
+  end
