b16f64e60a10af60dd8d006fbb8f6c98c639b838
LimanRPG <liman.vita@gmail.com>
2016-06-11 18:10:38 +0300

    Area adding files in question

--- spec/feature/questions/add_files_to_question_spec.rb
+++ spec/feature/questions/add_files_to_question_spec.rb
@@ -13,12 +13,23 @@ feature 'Add files to question', %q{
       visit new_question_path
     end
 
-    scenario 'User can add file when ask question' do
+    scenario 'User can add files when ask question', js: true do
       fill_in 'Title', with: 'TitleText'
       fill_in 'Body', with: 'BodyText'
-      attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+
+      within all('.nested-fields').first do
+        attach_file 'File', "#{Rails.root}/spec/spec_helper.rb"
+      end
+
+      click_on 'Add file'
+
+      within all('.nested-fields').last do
+        attach_file 'File', "#{Rails.root}/spec/rails_helper.rb"
+      end
+
       click_on 'Create'
 
       expect(page).to have_link 'spec_helper.rb', href: '/uploads/attachment/file/1/spec_helper.rb'
+      expect(page).to have_link 'rails_helper.rb', href: '/uploads/attachment/file/2/rails_helper.rb'
     end
   end
