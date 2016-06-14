51bf954e355663dfd56f79c65a595cec0b26b571
LimanRPG <liman.vita@gmail.com>
2016-06-15 00:05:36 +0300

    Add dependet: :destroy

--- app/models/answer.rb
+++ app/models/answer.rb
@@ -1,7 +1,7 @@
 class Answer < ActiveRecord::Base
   belongs_to :question
   belongs_to :user
-  has_many :attachments , as: :attachable
+  has_many :attachments , as: :attachable, dependent: :destroy
 
   validates :body, :question_id, :user_id, presence: true
   validates :body, length: { minimum: 5 }
