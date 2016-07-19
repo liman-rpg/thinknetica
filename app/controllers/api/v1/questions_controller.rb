379265d18250cb39839838bf34494cbe481f2017
LimanRPG <liman.vita@gmail.com>
2016-07-19 18:08:23 +0300

    API GET #answers

--- app/controllers/api/v1/questions_controller.rb
+++ app/controllers/api/v1/questions_controller.rb
@@ -1,11 +1,23 @@
 class Api::V1::QuestionsController < Api::V1::BaseController
+  before_action :load_question, only: [ :show, :answers ]
+
   def index
     @questions = Question.all
     respond_with @questions, each_serializer: QuestionCollectionSerializer
   end
 
   def show
-    @question = Question.find(params[:id])
     respond_with @question
+  end
+
+  def answers
+    # иначе в спеках при проверке answers, первый answer будет последним (*1)
+    respond_with @question.answers.order( created_at: :asc )
+  end
+
+  private
+
+  def load_question
+    @question = Question.find(params[:id])
   end
 end
--- spec/api/v1/questions_spec.rb
+++ spec/api/v1/questions_spec.rb
@@ -43,18 +43,6 @@ describe 'Questions API' do
       it 'question object contain short_title' do
         expect(response.body).to be_json_eql(question.title.truncate(10).to_json).at_path("questions/0/short_title")
       end
-
-      context 'answers' do
-        it 'included in question object' do
-          expect(response.body).to have_json_size(1).at_path("questions/0/answers")
-        end
-
-        %w(id body created_at updated_at).each do |attr|
-          it "contains #{ attr }" do
-            expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("questions/0/answers/0/#{ attr }")
-          end
-        end
-      end
     end
   end
 
@@ -113,6 +101,38 @@ describe 'Questions API' do
 
         it 'contain url in file' do
           expect(response.body).to be_json_eql(attachment.file.url.to_json).at_path("question/attachments/0/file/url")
+        end
+      end
+    end
+  end
+
+  describe 'GET #answer' do
+    context 'unauthorized' do
+      it 'returns 401 status if there is no access_token' do
+        get '/api/v1/questions/0/answers', format: :json
+        expect(response.status).to eq 401
+      end
+
+      it 'returns 401 status if access_token no vailid' do
+        get '/api/v1/questions/0/answers', format: :json, access_token: '1234'
+        expect(response.status).to eq 401
+      end
+    end
+
+    context 'authorized' do
+      let!(:question) { create(:question) }
+      let!(:answers)  { create_list(:answer, 3, question: question) }
+      let(:answer)    { answers.first }
+
+      before { get "/api/v1/questions/#{ question.id }/answers", format: :json, access_token: access_token.token }
+
+      it 'returns list of answers for question' do
+        expect(response.body).to have_json_size(3)
+      end
+      # (*1)
+      %w(id body created_at updated_at).each do |attr|
+        it "contains #{ attr }" do
+          expect(response.body).to be_json_eql(answer.send(attr.to_sym).to_json).at_path("0/#{ attr }")
         end
       end
     end
