67454e258cfd41af5a9e2c4ef7de5dfa36911843
LimanRPG <liman.vita@gmail.com>
2016-08-06 12:49:52 +0300

    Create model Subscription + association

--- app/models/user.rb
+++ app/models/user.rb
@@ -7,6 +7,7 @@ class User < ActiveRecord::Base
   has_many :votes, dependent: :destroy
   has_many :comments, dependent: :destroy
   has_many :authorizations, dependent: :destroy
+  has_many :subscriptions, dependent: :destroy
 
   def author_of?(object)
     id==object.user_id
