d524890338f1b865522fc73a9204ee549888c9fe
LimanRPG <liman.vita@gmail.com>
2016-08-06 15:30:17 +0300

    Test commit

--- app/models/concerns/subscriptable.rb
+++ app/models/concerns/subscriptable.rb
@@ -3,5 +3,14 @@ module Subscriptable
 
   included do
     has_many :subscriptions, as: :subscriptable, dependent: :destroy
+
+    after_create :create_subscription
+    # after_commit :create_subscription, on: :create
+
+    private
+
+    def create_subscription
+      Subscription.create(user: self.user, subscriptable: self)
+    end
   end
 end
