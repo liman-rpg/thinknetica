17211fed4e39c8d4e606a210a5a99b3d92c89dd1
LimanRPG <liman.vita@gmail.com>
2016-09-21 01:25:20 +0300

    Add sphinx production config

--- config/deploy.rb
+++ config/deploy.rb
@@ -32,7 +32,7 @@ set :default_shell, '/bin/bash -l'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{ config/database.yml config/private_pub.yml config/private_pub_thin.yml .env }
+set :linked_files, %w{ config/database.yml config/private_pub.yml config/private_pub_thin.yml config/sphinx.yml .env }
 
 # Default value for linked_dirs is []
 set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system public/uploads}
